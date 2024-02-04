534eae012a41c8a6affaa0bcf7dfe0758ca87eb5
Ivan Bolshakov <is.bolshakov@mail.ru>
2024-02-04 22:05:20 +0300

    fix: minor fix in default commands

--- config_data/config.py
+++ config_data/config.py
@@ -23,7 +23,7 @@ API_HOST_WEATHER = "tomorrow-io1.p.rapidapi.com"
 # поддерживаемые команды
 DEFAULT_COMMANDS = [
     ('start', 'Start bot'),
-    ('current_weather', 'Get current weather at your location'),
+    ('current_weather', 'Get hourly weather forecast at your location'),
     ('weather_forecast', 'Get weather forecast for the next 6 days at your location (including today)'),
     ('help', 'List of commands and their descriptions'),
     ('custom', 'Custom weather forecast (try \\custom for more information)'),
