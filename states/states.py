d05176ccf03d2339fea9c4d32e5acbe614b9786a
Ivan Bolshakov <is.bolshakov@mail.ru>
2024-02-05 02:20:22 +0300

    fix: removed unnecessary lines

--- states/states.py
+++ states/states.py
@@ -1,19 +1,5 @@
-import telebot
-from telebot.handler_backends import State, StatesGroup #States
-from config_data.config import BOT_TOKEN
-
-# States storage
-from telebot.storage import StateMemoryStorage
-
-# Now, you can pass storage to bot.
-state_storage = StateMemoryStorage() # you can init here another storage
-
-bot = telebot.TeleBot(BOT_TOKEN, state_storage=state_storage)
+from telebot.handler_backends import State, StatesGroup
 
 
-# States group.
 class MyStates(StatesGroup):
-    # Just name variables differently
-    forecast = State() # creating instances of State class is enough from now
-
-
+    forecast = State()
