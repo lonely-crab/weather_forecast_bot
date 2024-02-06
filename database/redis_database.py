b7618dbd6c13460eef9466177f3f20b3edd305bc
Ivan Bolshakov <is.bolshakov@mail.ru>
2024-02-06 22:37:32 +0300

    feat: updated redis db -> added user separation

---
+++ database/redis_database.py
@@ -0,0 +1,42 @@
+import redis
+from typing import Any
+import json
+
+
+redis_db = redis.Redis(host='localhost', port=6379, db=0)
+
+
+def _set_redis(user_id: str, key: str, value: dict | str) -> None:
+    value = {key: value}
+    if redis_db.exists(user_id):
+        value = json.loads(redis_db.get(user_id)).update({key: value})
+    new_value = json.dumps(value)
+    redis_db.set(user_id, new_value)
+
+
+def _get_redis(user_id: str, key: str) -> Any:
+    value = json.loads(redis_db.get(user_id)).get(key)
+    return value
+
+
+def _delete_redis(user_id: str) -> None:
+    redis_db.delete(user_id)
+
+
+class RedisDatabaseInterface:
+    @classmethod
+    def set_user(cls, user_id) -> None:
+        redis_db.set(user_id, '')
+
+    @classmethod
+    def set_redis(cls, user_id, key, value) -> None:
+        _set_redis(user_id, key, value)
+
+    @classmethod
+    def get_redis(cls, user_id, key) -> Any:
+        return _get_redis(user_id, key)
+
+    @classmethod
+    def delete_redis(cls, user_id) -> None:
+        _delete_redis(user_id)
+
