7e9caf48412ce5eff1f2cf906c413c4ccf633b8e
Ivan Bolshakov <is.bolshakov@mail.ru>
2024-02-07 21:56:03 +0300

    fix: fixes to work with different users

--- api/get_weather.py
+++ api/get_weather.py
@@ -1,6 +1,7 @@
 import requests
 from config_data.config import API_HOST_WEATHER, API_KEY_WEATHER
 from database.redis_database import RedisDatabaseInterface
+import json
 
 
 def _get_weather(user_id, timesteps: str = "1d", units: str = "metric") -> dict:
@@ -8,13 +9,17 @@ def _get_weather(user_id, timesteps: str = "1d", units: str = "metric") -> dict:
         location = RedisDatabaseInterface.get_redis(user_id, "location")
     except KeyError:
         raise ValueError("Location isn't set. Use /set_location command.")
-
+    try:
+        location_query = ','.join([str(location['latitude']), str(location['longitude'])])
+    except TypeError:
+        location_query = location
     headers = {
         "X-RapidAPI-Key": API_KEY_WEATHER,
         "X-RapidAPI-Host": API_HOST_WEATHER
     }
+
     params = {
-        "location": ','.join([str(location['latitude']), str(location['longitude'])]),
+        "location": location_query,
         "timesteps": timesteps,
         "units": units
     }
@@ -30,10 +35,11 @@ def _get_weather(user_id, timesteps: str = "1d", units: str = "metric") -> dict:
     
 class GetWeatherInterface:
     @classmethod
-    def get_weather(cls,user_id, timesteps: str = "1d", units: str = "metric") -> dict:
+    def get_weather(cls, user_id, timesteps: str = "1d", units: str = "metric") -> dict:
         try:
             if not _get_weather(user_id, timesteps, units):
-                raise ValueError("Couldn't get weather from API.")
+                raise ValueError("Couldn't get weather from API."
+                                 " Check if your location is correct or try locating differently.")
             return _get_weather(user_id, timesteps, units)
         except ValueError as error:
             raise ValueError(str(error))
